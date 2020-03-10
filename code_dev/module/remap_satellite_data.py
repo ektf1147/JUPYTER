0f4095445988577dda441fe73fb8a2639955b304
ektf1147 <jhlee13@yonsei.ac.kr>
2020-03-10 12:16:20 +0900

    modifed remap with KDTree

--- code_dev/module/remap_satellite_data.py
+++ code_dev/module/remap_satellite_data.py
@@ -1,30 +1,41 @@
-# remap.py
+# remap_satellite_data.py
 
-import math
+# for array manimulation
 import numpy as np
 
-def haversine(lat1, lon1, lat2, lon2):
-    rad = math.pi / 180  # degree to radian
-    R = 6378.1  # earth average radius at equador (km)
-    dlon = (lon2 - lon1) * rad
-    dlat = (lat2 - lat1) * rad
-    a = (math.sin(dlat / 2)) ** 2 + math.cos(lat1 * rad) * \
-        math.cos(lat2 * rad) * (math.sin(dlon / 2)) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    d = R * c
-    return d
+# for interpolation
+from scipy.spatial import cKDTree
 
-def idwr(x, y, z, xi, yi):
-    lstxyzi = []
-    for p in range(len(xi)):
-        lstdist = []
-        for s in range(len(x)):
-            d = (haversine(x[s], y[s], xi[p], yi[p]))
-            lstdist.append(d)
-        sumsup = list((1 / np.power(lstdist, 2)))
-        suminf = np.sum(sumsup)
-        sumsup = np.sum(np.array(sumsup) * np.array(z))
-        u = sumsup / suminf
-        xyzi = [xi[p], yi[p], u]
-        lstxyzi.append(xyzi)
-    return lstxyzi
+# This is the function used for converting lat/lon to x, y, z:
+def lon_lat_to_cartesian(lon, lat, R = 1):
+    '''
+    Calculates lon, lat of a point on a sphere with 
+    radius R    
+    '''
+    
+    lon_r = np.radians(lon)
+    lat_r = np.radians(lat)
+    
+    x = R * np.cos(lat_r) * np.cos(lon_r)
+    y = R * np.cos(lat_r) * np.sin(lon_r)
+    z = R * np.sin(lat_r)
+    
+    return x, y, z
+
+
+def idw_cKDTree(lon_s, lat_s, val_s, lon_t, lat_t):
+    
+    # convert lat/lon to Cartesian grid.
+    xs, ys, zs = lon_lat_to_cartesian(lon_s.flatten(), lat_s.flatten())
+    xt, yt, zt = lon_lat_to_cartesian(lon_t.flatten(), lat_t.flatten()) 
+    
+    # create cKDTree object to represent source grid.
+    tree = cKDTree(list(zip(xs, ys, zs)))
+    
+    # Interpolate using inverse distance weighting, using 10 nearest neighbors.
+    d, inds = tree.query(list(zip(xt, yt, zt)), k = 10)
+    w = 1.0 / d**2
+    interp_idw = np.sum(w * val_s.flatten()[inds], axis=1) / np.sum(w, axis = 1)
+    interp_idw.shape = lon_t.shape
+    
+    return interp_idw
